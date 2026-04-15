eefd022c835eaa0ca98733a7d216d22f81e4651e
Miloni Shah <mmshah0304@gmail.com>
2026-04-15 17:10:02 +0530

    Added default exports in empty pages

--- readflow-backend/src/main.ts
+++ readflow-backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
